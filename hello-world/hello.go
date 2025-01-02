dce9be7d84db4b47aef6b3d79707d47419fa592f
Alex Kräuchi <al@soup.one>
2025-01-02 17:26:20 +0100

    Refactor Hello

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,23 +2,35 @@ package main
 
 import "fmt"
 
+const (
+	spanish = "ES"
+	french  = "FR"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+// Public functions start with a capital letter, private functions start with a lowercase letter
 func Hello(name, lang string) string {
-	var prefix string
-
-	switch lang {
-	case "FR":
-		prefix = "Bonjour, "
-	case "ES":
-		prefix = "Hola, "
-	default:
-		prefix = "Hello, "
-	}
-
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(lang) + name
+}
 
-	return prefix + name
+// Return type is 'named return value'. If the variable it not assigned it will default to empty string
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	// named return values don't need to be explicitly returned
+	return
 }
 
 func main() {
