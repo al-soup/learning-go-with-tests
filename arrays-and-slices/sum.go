1d5bb6e18b3b05313064f3e6aa9d9b0179cb1b1b
Alex Kräuchi <al@soup.one>
2025-01-02 18:41:40 +0100

    Use slice instead of array

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,8 +1,9 @@
 package main
 
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers []int) (sum int) {
+	// range returns the index (_) and the value by iterating over the array
+	for _, number := range numbers {
+		sum += number
 	}
 	return
 }
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -4,14 +4,28 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	// define the length of the array with [5]int or infer the fixed length with [...]int
-	// []int for a slice (variable length)
-	numbers := [5]int{1, 2, 3, 4, 5}
+	// []int for a slice (variable length). A function with a slice parameter will not compile with an array argument
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		// print default format %v which works well for arrays
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			// print default format %v which works well for arrays
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
