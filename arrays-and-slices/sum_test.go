6e53dddaac216505f3a73541ba16f35643ffac62
Alex Kräuchi <al@soup.one>
2025-01-02 18:26:47 +0100

    Init arrays and slices

---
+++ arrays-and-slices/sum_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	// define the length of the array with [5]int or infer the fixed length with [...]int
+	// []int for a slice (variable length)
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		// print default format %v which works well for arrays
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
