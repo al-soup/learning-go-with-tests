6cbb51a56a1184b21eb9386d064dfe5e5596b6cc
Alex Kräuchi <alex.kraeuchi@swisscom.com>
2025-03-26 08:50:55 +0100

    Refactor sum function

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -2,16 +2,20 @@ package main
 
 import "testing"
 
+// define the length of the array with [5]int or infer the fixed length with [...]int
+// []int for a slice (variable length). A function with a slice parameter will not compile with an arra y argument
+
 func TestSum(t *testing.T) {
-	// define the length of the array with [5]int or infer the fixed length with [...]int
-	// []int for a slice (variable length). A function with a slice parameter will not compile with an array argument
-	numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	if got != want {
-		// print default format %v which works well for arrays
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
