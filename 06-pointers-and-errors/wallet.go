3589dd0adefd0a69ad7108d9637b4365c9309d8f
Alex Kräuchi <alex.kraeuchi@swisscom.com>
2025-05-16 16:35:22 +0200

    Implement Stringer interface

--- 06-pointers-and-errors/wallet.go
+++ 06-pointers-and-errors/wallet.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
+type BitCoin int
+
+// The Stringer interface is defined in the fmt package
+type Stringer interface {
+	String() string
+}
+
+// Creating a method on a type declaration is the same as it is on a struct
+func (b BitCoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	balance int
+	balance BitCoin
 }
 
 // In Go, value receivers `(func (w Wallet))` create a copy of the struct, so modifications
@@ -11,12 +23,12 @@ type Wallet struct {
 // original struct, allowing permanent modifications. Use value receivers for immutability
 // and small structs, and pointer receivers when you need to modify the struct or avoid
 // copying large data structures
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
 
 // When you call a function or a method the arguments are copied
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount BitCoin) {
 	// Using the ampersand operator (&) to get a pointer to a variable and print it with %p
 	fmt.Printf("address of balance in Deposit is %p \n", &w)
 	w.balance += amount
--- 06-pointers-and-errors/wallet_test.go
+++ 06-pointers-and-errors/wallet_test.go
@@ -7,13 +7,15 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(BitCoin(10))
 
 	got := wallet.Balance()
 
-	want := 10
+	want := BitCoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		// We change the "verb" to to string so the `String()` method of the type is used.
+		// t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
