572f30e7a4bdbd4657f918776c361d6ecafa7d05
Alex Kräuchi <alex.kraeuchi@swisscom.com>
2025-05-19 20:59:09 +0200

    Move helpers outside of test function

--- 06-pointers-and-errors/wallet_test.go
+++ 06-pointers-and-errors/wallet_test.go
@@ -4,32 +4,6 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	assertBalance := func(t testing.TB, wallet Wallet, want BitCoin) {
-		got := wallet.Balance()
-
-		if got != want {
-			// We change the "verb" to to string so the `String()` method of the type is used.
-			// t.Errorf("got %d want %d", got, want)
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-
-		if got == nil {
-			// stops execution here
-			t.Fatal("did not receive expected errror")
-		}
-
-		// `got.Error()` converts Error to a string
-		// It makes sense to check against nil first (above)
-		// if got.Error() != want {
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(BitCoin(10))
@@ -50,5 +24,31 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingFund)
 	})
+}
 
+// Move helpers outside of test body so you can start reading assertions right away
+func assertBalance(t testing.TB, wallet Wallet, want BitCoin) {
+	got := wallet.Balance()
+
+	if got != want {
+		// We change the "verb" to to string so the `String()` method of the type is used.
+		// t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+
+	if got == nil {
+		// stops execution here
+		t.Fatal("did not receive expected errror")
+	}
+
+	// `got.Error()` converts Error to a string
+	// It makes sense to check against nil first (above)
+	// if got.Error() != want {
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
