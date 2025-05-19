00fcda2e44fc702a7cf2d267782bb28418724b9e
Alex Kräuchi <alex.kraeuchi@swisscom.com>
2025-05-19 08:47:26 +0200

    Add withdraw method

--- 06-pointers-and-errors/wallet.go
+++ 06-pointers-and-errors/wallet.go
@@ -33,3 +33,7 @@ func (w *Wallet) Deposit(amount BitCoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w)
 	w.balance += amount
 }
+
+func (w *Wallet) Withdraw(amount BitCoin) {
+	w.balance -= amount
+}
--- 06-pointers-and-errors/wallet_test.go
+++ 06-pointers-and-errors/wallet_test.go
@@ -5,17 +5,35 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(BitCoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
+		wallet.Deposit(BitCoin(10))
 
-	want := BitCoin(10)
+		got := wallet.Balance()
 
-	if got != want {
-		// We change the "verb" to to string so the `String()` method of the type is used.
-		// t.Errorf("got %d want %d", got, want)
-		t.Errorf("got %s want %s", got, want)
-	}
+		want := BitCoin(10)
+
+		if got != want {
+			// We change the "verb" to to string so the `String()` method of the type is used.
+			// t.Errorf("got %d want %d", got, want)
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: BitCoin(10)}
+
+		wallet.Withdraw(5)
+
+		got := wallet.Balance()
+
+		want := BitCoin(5)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	})
 }
