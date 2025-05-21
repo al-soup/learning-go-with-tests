a5087a75142b1ecece258856772644c3c1589886
al-soup <al@soup.one>
2025-05-21 17:38:33 +0200

    Move DI to right directory

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
-func Countdown(out *bytes.Buffer) {
+func Countdown(out io.Writer) {
+	//fmt.Fprint takes an io.Writer (like *bytes.Buffer) and sends a string to it
 	fmt.Fprint(out, "3")
 }
