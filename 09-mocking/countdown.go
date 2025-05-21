ca21176c4ef2636a96a8525845f5bf4e2048d8ea
al-soup <al@soup.one>
2025-05-21 17:44:27 +0200

    Send text to different write for actual implementation

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Countdown(out io.Writer) {
 	//fmt.Fprint takes an io.Writer (like *bytes.Buffer) and sends a string to it
 	fmt.Fprint(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
