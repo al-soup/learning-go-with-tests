19f515732124b80ed97128980ee6d9d9265b6d77
al-soup <al@soup.one>
2025-05-21 17:49:34 +0200

    Print sequence in lines

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -7,8 +7,11 @@ import (
 )
 
 func Countdown(out io.Writer) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
 	//fmt.Fprint takes an io.Writer (like *bytes.Buffer) and sends a string to it
-	fmt.Fprint(out, "3")
+	fmt.Fprint(out, "Go!")
 }
 
 func main() {
