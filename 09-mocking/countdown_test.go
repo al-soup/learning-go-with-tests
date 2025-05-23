8177e5cc7535714f2c781601a84286b68b73c462
al-soup <al@soup.one>
2025-05-23 10:27:46 +0200

    Rewrite test for printing

--- 09-mocking/countdown_test.go
+++ 09-mocking/countdown_test.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	spySleepPrinter := &SpyCountdownOperations{}
+	t.Run("sleep before every sprint", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
 
-	Countdown(spySleepPrinter, spySleepPrinter)
-	want := []string{
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-	}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
 
-	if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
-		t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
-	}
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+
+	t.Run("prints number 3 to 1 and the 'Go!'", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		buffer := &bytes.Buffer{}
+
+		Countdown(buffer, spySleepPrinter)
+		want := `3
+2
+1
+Go!`
+		got := buffer.String()
+
+		if want != got {
+			t.Errorf("want %q, got %q", want, got)
+		}
+
+	})
 }
