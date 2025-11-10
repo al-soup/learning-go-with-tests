f1d63ec4b254f33d2e213e3efa72c5f695464060
al-soup <al@soup.one>
2025-11-10 11:21:52 +0100

    Update comment

--- 11-select/websiteRacer.go
+++ 11-select/websiteRacer.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
+	// With `select` the first channel wins and execution continues with the first case that is ready.
 	select {
 	case <-ping(a):
 		return a
@@ -15,6 +16,9 @@ func Racer(a, b string) (winner string) {
 
 func ping(url string) (ch chan bool) {
 	// ch = make(chan struct{}) would be more memory efficient since nothing needs to actually be allocated
+	// In this case the type which is sent to the channel does not matter. We just need a signal, so struct would be sufficient.
+	// Always `make` channels. If you use `var ch chan struct{}` it will be nil because the variable would be implemented as "zero" value
+	// <- would block forever because you cannot send nil to a channel
 	ch = make(chan bool)
 	go func() {
 		http.Get(url)
