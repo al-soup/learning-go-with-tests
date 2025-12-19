1f9413821d83ac5fd0802a4ed671ef70c92d71d5
al-soup <al@soup.one>
2025-12-19 14:32:24 +0100

    feat: Parse Roman back to Arabic

--- 15-property-based-tests/numeral_test.go
+++ 15-property-based-tests/numeral_test.go
@@ -3,43 +3,46 @@ package propertybasedtests
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{Arabic: 1, Roman: "I"},
+	{Arabic: 2, Roman: "II"},
+	{Arabic: 3, Roman: "III"},
+	{Arabic: 4, Roman: "IV"},
+	{Arabic: 5, Roman: "V"},
+	{Arabic: 6, Roman: "VI"},
+	{Arabic: 7, Roman: "VII"},
+	{Arabic: 8, Roman: "VIII"},
+	{Arabic: 9, Roman: "IX"},
+	{Arabic: 10, Roman: "X"},
+	{Arabic: 14, Roman: "XIV"},
+	{Arabic: 18, Roman: "XVIII"},
+	{Arabic: 20, Roman: "XX"},
+	{Arabic: 39, Roman: "XXXIX"},
+	{Arabic: 40, Roman: "XL"},
+	{Arabic: 47, Roman: "XLVII"},
+	{Arabic: 49, Roman: "XLIX"},
+	{Arabic: 50, Roman: "L"},
+	{Arabic: 100, Roman: "C"},
+	{Arabic: 90, Roman: "XC"},
+	{Arabic: 400, Roman: "CD"},
+	{Arabic: 500, Roman: "D"},
+	{Arabic: 900, Roman: "CM"},
+	{Arabic: 1000, Roman: "M"},
+	{Arabic: 1984, Roman: "MCMLXXXIV"},
+	{Arabic: 3999, Roman: "MMMCMXCIX"},
+	{Arabic: 2014, Roman: "MMXIV"},
+	{Arabic: 1006, Roman: "MVI"},
+	{Arabic: 798, Roman: "DCCXCVIII"},
+}
+
 func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Roman  string
-	}{
-		{Arabic: 1, Roman: "I"},
-		{Arabic: 2, Roman: "II"},
-		{Arabic: 3, Roman: "III"},
-		{Arabic: 4, Roman: "IV"},
-		{Arabic: 5, Roman: "V"},
-		{Arabic: 6, Roman: "VI"},
-		{Arabic: 7, Roman: "VII"},
-		{Arabic: 8, Roman: "VIII"},
-		{Arabic: 9, Roman: "IX"},
-		{Arabic: 10, Roman: "X"},
-		{Arabic: 14, Roman: "XIV"},
-		{Arabic: 18, Roman: "XVIII"},
-		{Arabic: 20, Roman: "XX"},
-		{Arabic: 39, Roman: "XXXIX"},
-		{Arabic: 40, Roman: "XL"},
-		{Arabic: 47, Roman: "XLVII"},
-		{Arabic: 49, Roman: "XLIX"},
-		{Arabic: 50, Roman: "L"},
-		{Arabic: 100, Roman: "C"},
-		{Arabic: 90, Roman: "XC"},
-		{Arabic: 400, Roman: "CD"},
-		{Arabic: 500, Roman: "D"},
-		{Arabic: 900, Roman: "CM"},
-		{Arabic: 1000, Roman: "M"},
-		{Arabic: 1984, Roman: "MCMLXXXIV"},
-		{Arabic: 3999, Roman: "MMMCMXCIX"},
-		{Arabic: 2014, Roman: "MMXIV"},
-		{Arabic: 1006, Roman: "MVI"},
-		{Arabic: 798, Roman: "DCCXCVIII"},
-	}
+
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
@@ -47,5 +50,38 @@ func TestRomanNumerals(t *testing.T) {
 				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+// With property based test you have a good enough understanding of the domain
+// That you can use random values as test properties
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		// We can only represent arabic values up to here
+		if arabic > 3999 {
+			return true
+		}
+		// use `go test -v` to see the output
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(int(arabic))
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == int(arabic)
+	}
+
+	// Provide a function that it will run against a number of random inputs (100 times by default)
+	// You could configure that by passing a config as 2nd argument: `&quick.Config{ MaxCount: 1000` }`
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
